a9eebf2b91c37fa3a1b27eb90346378b9c85635c
AFCC28 <142193041+af-coronel@users.noreply.github.com>
2023-10-10 11:35:51 -0300

    Se agregaron tests a yvytuHome, se actualizaron test anteriores, se agrego textos_yvytu.json al fixture

--- cypress/PageObjectModel/yvytuPages/yvytuHome.js
+++ cypress/PageObjectModel/yvytuPages/yvytuHome.js
@@ -1,5 +1,12 @@
 class YvytuHome{
-
+    /* La estrategia ideal para el armado de POM es: 
+    1. Ordenar los métodos del objeto definido considerado
+    el orden de los elementos que aparecen a medida que
+    recorremos la página.
+    2. Poner comentarios que indiquen a qué elementos
+    se refieren los métodos del objeto */
+    
+    
     //Botones del Header
     getMenuPillButtons(){
         return cy.get('a[class*="rounded-full py-2 px-4"]');
@@ -9,23 +16,65 @@ class YvytuHome{
         return cy.get("nav#menu-nav a");
       }
 
-    //Boton ir arriba
-    getIrArribaButton() {
-    return cy.get("#btn-scroll-top");
+
+    //Banner de imagen home
+    getCurrentImageBanner() {
+        return cy.get(
+          `[class^="w-full h-600 bg-[url('/public/images/header-gallery/"][class*="slick-current"]`
+        );
       }
     
-    //Banner de imagen home
+    getImagenesBanner() {
+        return cy.get(`[class*="bg-[url('/public/images/header-gallery/"]:visible`);
+      }
+    
+    getImgButton() {
+        return cy.get("ul").first().find("li button");
+      }
+
     getGenericSubtitle() {
         return cy.get("h2");
       }
 
+    getGenericParrafo() {
+        return cy.get(".text-justify p");
+      }  
+
     getGenericButton() {
         return cy.get("a");
       }
+    
 
-    getImgCabaniaYaguarete() {
-        return cy.get("#slick-slide00");
+    // Seccion de texto //
+
+    //Boton ir arriba
+    getIrArribaButton() {
+        return cy.get("#btn-scroll-top");
       }  
+
+    //Reel de Imágenes
+    getReelImagenes() {
+    return cy.get('[class="slick-list draggable"]').eq(1).find("img");
+      }
+
+    //Cabañas
+    getImgCabaniaYaguarete() {
+    return cy.get("#slick-slide00");
+      }
+
+    getImgCabaniaArasari() {
+    return cy.get("#slick-slide10");
+      }
+      
+    
+    //Elementos no testeados en esta instancia:
+
+    //Como llegar
+    //Contacto
+    //Colaborá
+    //Sponsor
+    //Redes
+    //Footer  
 };
 
 export default new YvytuHome();
--- cypress/e2e/Yvytu/yvytuHome.cy.js
+++ cypress/e2e/Yvytu/yvytuHome.cy.js
@@ -7,7 +7,7 @@ const YvytuHome = require ("../../PageObjectModel/yvytuPages/yvytuHome");
         cy.visit("https://vientosdelaselva.com.ar/");
 
       });
-
+      
     it("Verificar barra de navegación - Iterar en Botones píldora", () => {
         const menu = [
             "LA RESERVA", 
@@ -49,6 +49,33 @@ const YvytuHome = require ("../../PageObjectModel/yvytuPages/yvytuHome");
         });
       });
 
+    it("Verificar Imágenes del Banner Principal", () => {
+        //El texto es /public/images/header-gallery/01.png y varía hasta 04
+        const bannerList = ["01.png", "02.png", "03.png", "04.png"];
+    
+        bannerList.forEach((banner, inx) => {
+          YvytuHome
+            .getCurrentImageBanner()
+            .should(
+              "have.class",
+              `bg-[url('/public/images/header-gallery/${banner}')]`
+            );
+    
+          YvytuHome
+            .getImgButton()
+            .its("length")
+            .then((cantidad) => {
+              if (cantidad != inx + 1) {
+                YvytuHome
+                  .getImgButton()
+                  .eq(inx + 1)
+                  .click();
+                cy.wait(1000);
+              }
+            });
+        });
+      });
+
     it("Verificar comportamiento del botón Ir Arriba", () => { 
        YvytuHome
          .getIrArribaButton()
@@ -61,6 +88,7 @@ const YvytuHome = require ("../../PageObjectModel/yvytuPages/yvytuHome");
 
        YvytuHome
          .getIrArribaButton()
+         .wait(1000) // Espera 1 segundo
          .should("be.visible")
          .and("have.text",
           "\n            Ir arriba\n          ")
@@ -75,6 +103,53 @@ const YvytuHome = require ("../../PageObjectModel/yvytuPages/yvytuHome");
             );
          })
       });
+
+    it("Verificar textos de la página", () => {
+      let inxPar = 0;
+    
+        //Leer el readme
+        cy.fixture("textos_yvytu")
+          .then((txt_yvytu) => {
+
+        //Se toma cada elemento definido dentro del arrayJson que está en fixtures
+        //Se anidan forEach en forEach 
+          txt_yvytu.forEach((elTexto, inx) => {
+          cy.log(`**VALIDACIÓN DEL TITULO: ${inx + 1}**`);
+
+          let yvyTitulo = elTexto.titulo;
+
+        //Se splitea el título del JSON con espacio para tomar cada palabra individual
+            yvyTitulo = yvyTitulo.split(" ");
+            yvyTitulo.forEach((palabra) => {
+            YvytuHome
+              .getGenericSubtitle()
+              .eq(inx + 1)
+              .should("contain.text", palabra);
+        });
+    
+          //Verificar Párrafos
+           let yvyParrafos = elTexto.parrafos;
+
+          //"Parrafos" en el json contiene multiples parrafos
+            yvyParrafos.forEach((elParrafo) => {
+            cy.log(`Validar Parrafo ${inxPar}: ${elParrafo}`);
+    
+            YvytuHome
+              .getGenericParrafo()
+              .eq(inxPar)
+              .invoke("text")
+              .then((parr) => {
+                  cy.log(`Parrafo sin modificar: ${parr}`);
+                  parr = parr.replace(/\s+/g, " ").trim();
+                  cy.log(`Parrafo modificado: ${parr}`);
+                  expect(parr).to.include(elParrafo);
+                });
+              inxPar++;
+            });
+          });
+        });
+      });
+
     //Para testear atributos usamos have.attr, atributo, valor. 
     // "a" denota un enlace funcional, en html. 
     it("Verificar Botón de Reservar", () => {
@@ -93,11 +168,125 @@ const YvytuHome = require ("../../PageObjectModel/yvytuPages/yvytuHome");
           );
       });
 
-      it.only("Verificar Imagenes de las Cabañas", () => {
-        YvytuHome
-          .getImgCabaniaYaguarete()
-          .should("have.attr", "src", "./public/images/cabana-gallery/01.png")
+    it("Verificar Reel de Imágenes", () => {
+      let arrayImagenes = [
+          "frase01.png",
+          "noche.png",
+          "pasafauna.png",
+          "picaflor.png",
+          "carpincho.png",
+          "frase01.png",
+          "noche.png",
+          "pasafauna.png",
+          "picaflor.png",
+          "carpincho.png",
+          "frase01.png",
+          "noche.png",
+          "pasafauna.png",
+          "picaflor.png",
+        ];
+    
+      YvytuHome.getReelImagenes().each((imagenes, index) => {
+        cy.wrap(imagenes).should(
+        "have.attr",
+        "src",
+        `./public/images/gallery/${arrayImagenes[index]}`
+        );
+       });
       });
 
+    it("Verificar Imagenes de las Cabañas", () => {
+     cy.log("**Todas las imagenes tienen como texto alternativo Imagen 1**");
+     cy.log("**YVYTU-015 Error texto alternativo**");
+      YvytuHome
+        .getImgCabaniaYaguarete()
+        .should("have.attr", "src", "./public/images/cabana-gallery/01.png")
+        .and("have.attr", "alt", "Foto de la fachada de la cabaña Yaguareté");
+      YvytuHome
+        .getImgCabaniaArasari()
+        .should("have.attr", "src", "./public/images/cabana-gallery/01.png")
+        .and("have.attr", "alt", "Foto de la fachada de la cabaña Yaguareté");
+      });
+    
+    it("Verificar Botón de Donar", () => {
+      YvytuHome
+        .getGenericButton()
+        .contains("Donar")
+        .should("have.attr", "href", "https://cafecito.app/reserva-yvytu")
+        .and("have.attr", "target", "_blank");
+    
+      YvytuHome
+        .getGenericButton()
+        .contains("Donar")
+        .should(
+         "have.css",
+         "Background",
+         "rgb(34, 153, 84) none repeat scroll 0% 0% / auto padding-box border-box"
+        );
+      });
+    
+    it("Visual Tetsing de Yvytu usando Snapshoot", () => {
+        cy.compareSnapshot("home-page");
+      });
+    
+    it("Visual Tetsing de Yvytu usando Snapshoot en un elemento", () => {
+        YvytuHome
+         .getMenuPillButtons()
+         .parent()
+         .compareSnapshot("botones");
+      });
+    
+    it.only("Visual testing del boton ir arriba", () => {
+        YvytuHome
+         .getIrArribaButton()
+         .should(
+          "have.css",
+          "background",
+          "rgb(34, 153, 84) none repeat scroll 0% 0% / auto padding-box border-box"
+        );
+      });
+    });
+      
 
-  });    
+    //Es mala práctica dejar el codigo comentado
+    //En este caso lo dejaré para estudiarlo antes de borrarlo
+    /* Código generador del Array para el Reel:
+    
+    La profe usó este código como puntapié para generar
+    el array con el que luego se armó el test para verificar
+    el ReelDeImagenes:
+
+      it("Verificar Reel de Imágenes", () => { 
+        let newArray = [];
+       YvytuHome
+      .getReelImagenes()
+      .each((imagen, index) => {
+        cy.wrap(imagen)
+          .invoke("attr", "src")
+          .then((texto) => {
+            cy.log(texto);
+            newArray.push(texto);
+          });
+      })
+      .then(() => {
+        cy.log(JSON.stringify(newArray));
+      });
+    });
+
+    //Generado y copypasteado en/desde Cypress:
+    log[
+    "./public/images/gallery/frase01.png",
+    "./public/images/gallery/noche.png",
+    "./public/images/gallery/pasafauna.png",
+    "./public/images/gallery/picaflor.png",
+    "./public/images/gallery/carpincho.png",
+    "./public/images/gallery/frase01.png",
+    "./public/images/gallery/noche.png",
+    "./public/images/gallery/pasafauna.png",
+    "./public/images/gallery/picaflor.png",
+    "./public/images/gallery/carpincho.png",
+    "./public/images/gallery/frase01.png",
+    "./public/images/gallery/noche.png",
+    "./public/images/gallery/pasafauna.png",
+    "./public/images/gallery/picaflor.png"] */
+       
